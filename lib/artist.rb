943792a7f87562cbed8578dd2384273c315b44c1
eselbeus <eselbeus@gmail.com>
2019-02-02 23:05:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
   attr_accessor :name
   attr_reader :songs
   
-  extend Memorable, Findable
+  extend Memorable::ClassMetods, Memorable::InstanceMethods, Findable
   include Paramable
 
   @@artists = []
